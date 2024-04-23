1547f7c2332c4b7c170a91206761c7d46fd9c22f
Nina Shestakova <nina.a.shestakova@gmail.com>
2024-04-23 11:12:11 +0200

    Added NavItems component and ChevronDown

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,10 +1,37 @@
-// "use client"
+"use client";
 
+import { PRODUCT_CATEGORIES } from "@/src/config";
 import { useState } from "react";
+import NavItem from "./NavItem";
 
 const NavItems = () => {
-  const [activeIndex, setActiveIndex] = useState();
-  return <div className="flex gap-4 h-full"></div>;
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
+  const isAnyOpen = activeIndex !== null;
+
+  return (
+    <div className="flex gap-4 h-full">
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null);
+          } else setActiveIndex(i);
+        };
+
+        const isOpen = i === activeIndex;
+
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isAnyOpen={isAnyOpen}
+            isOpen={isOpen}
+            key={category.value}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default NavItems;
