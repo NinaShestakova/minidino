3d8e88ba7c6f15e7a93e1013a8bd0529aa0fc077
Nina Shestakova <nina.a.shestakova@gmail.com>
2024-04-25 17:24:07 +0200

    Added Shipping transaction

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -8,8 +8,10 @@ import {
   SheetTrigger,
   Sheet,
 } from "./ui/sheet";
+import { Separator } from "@/src/components/ui/separator";
 
 const Cart = () => {
+  const itemCount = 1;
   return (
     <Sheet>
       <SheetTrigger className="group -m-2 flex items-center p-2">
@@ -25,6 +27,30 @@ const Cart = () => {
         <SheetHeader className="space-y-2.5 pr-6">
           <SheetTitle>Cart (0)</SheetTitle>
         </SheetHeader>
+
+        {itemCount > 0 ? (
+          <>
+            <div className="flex w-full flex-col pr-6">
+              {/* TODO: cart logic */}
+              cart items
+            </div>
+            <div className="space-y-4 pr-6">
+              <Separator />
+              <div className="space-y-1.5 text-sm">
+                <div className="flex">
+                  <span className="flex-1">Shipping</span>
+                  <span>Free</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Transaction Free</span>
+                  <span>Free</span>
+                </div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div></div>
+        )}
       </SheetContent>
     </Sheet>
   );
