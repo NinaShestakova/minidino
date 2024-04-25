67a1817093cde40c4455f5fe0300795ddafa571b
Nina Shestakova <nina.a.shestakova@gmail.com>
2024-04-25 12:46:52 +0200

    Added Sheet npx ui and Cart component

---
+++ src/components/Cart.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { Sheet } from "lucide-react";
+
+const Cart = () => {
+  return <Sheet></Sheet>;
+};
+
+export default Cart;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,12 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 import { Icons } from "./Icons";
 import NavItems from "./NavItems";
 import UseStateTest from "./UseStateTest";
+import { buttonVariants } from "./ui/button";
+import Cart from "./Cart";
 
 const Navbar = () => {
+  const user = null;
+
   return (
     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
       <header className="relative bg-white">
@@ -20,7 +24,52 @@ const Navbar = () => {
 
               <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
                 <NavItems />
-                {/* <UseStateTest /> */}
+              </div>
+              <div className="ml-auto flex items-center">
+                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justufy-end lg:space-x-6">
+                  {user ? null : (
+                    <a
+                      href="/sign-in"
+                      className={buttonVariants({
+                        variant: "ghost",
+                      })}
+                    >
+                      Sign in
+                    </a>
+                  )}
+
+                  {user ? null : (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  )}
+
+                  {user ? (
+                    <p></p>
+                  ) : (
+                    <a
+                      href="/sign-up"
+                      className={buttonVariants({ variant: "ghost" })}
+                    >
+                      Create account
+                    </a>
+                  )}
+
+                  {user ? (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  ) : null}
+
+                  {user ? null : (
+                    <div className="flex lg:ml-6">
+                      <span
+                        className="h-6 w-px bg-gray-200"
+                        aria-hidden="true"
+                      />
+                    </div>
+                  )}
+
+                  <div className="ml-4 flow-root lg:ml-6">
+                    <Cart />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
